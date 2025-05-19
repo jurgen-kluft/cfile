1cc434faa4f72e544d6da9b50f2e93fa905e8e8d
jurgen.kluft <jurgen.kluft@gmail.com>
2025-05-19 19:17:34 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -18,12 +18,12 @@ func GetPackage() *denv.Package {
 	mainpkg.AddPackage(ccorepkg)
 
 	// 'cfile' library
-	mainlib := denv.SetupDefaultCppLibProject("cfile", "github.com\\jurgen-kluft\\cfile")
-	mainlib.Dependencies = append(mainlib.Dependencies, ccorepkg.GetMainLib())
+	mainlib := denv.SetupCppLibProject("cfile", "github.com\\jurgen-kluft\\cfile")
+	mainlib.AddDependencies(ccorepkg.GetMainLib()...)
 
 	// 'cfile' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cfile_test", "github.com\\jurgen-kluft\\cfile")
-	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
+	maintest.AddDependencies(unittestpkg.GetMainLib()...)
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
